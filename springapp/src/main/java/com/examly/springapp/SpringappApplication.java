2a05efcc25614d909849429df2748bd3c5fca441
Soundariya-Babu <soundariyababu01@gmail.com>
2023-11-04 09:16:42 +0000

    sdsd

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -9,6 +9,7 @@ public class SpringappApplication {
 
 	public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Latest changes");
 		SpringApplication.run(SpringappApplication.class, args);
 	}
 }
